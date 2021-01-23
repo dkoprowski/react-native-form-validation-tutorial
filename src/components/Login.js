de34f381d2a2881df00fff1d7507cdcb488d2adf
Daniel Koprowski <contact@koprowski.it>
2021-01-23 15:13:49 +0100

    Add FormProvider to Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useForm } from 'react-hook-form'
+import { FormProvider, useForm } from 'react-hook-form'
 import { Button } from 'react-native'
 import styled from 'styled-components/native'
 
@@ -14,8 +14,10 @@ const Login = () => {
 
   return (
     <Wrapper>
-      <Input label='Username' />
-      <Input label='Password' error="Password can't be blank" />
+      <FormProvider {...formMethods}>
+        <Input label='Username' />
+        <Input label='Password' error="Password can't be blank" />
+      </FormProvider>
       <Button title='Login' />
     </Wrapper>
   )
