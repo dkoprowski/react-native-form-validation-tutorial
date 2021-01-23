182d887a3be70d3e53e09f406ce42b52e3525b5c
Daniel Koprowski <contact@koprowski.it>
2021-01-23 17:30:35 +0100

    Add rules and submit handlers

--- src/components/Login.js
+++ src/components/Login.js
@@ -17,13 +17,38 @@ const LOGIN_FIELDS = {
 const Login = () => {
   const formMethods = useForm()
 
+  const onSubmit = (form) => {
+    console.log(form)
+  }
+
+  const onErrors = (errors) => {
+    console.warn(errors)
+  }
+
   return (
     <Wrapper>
       <FormProvider {...formMethods}>
-        <FormInput name={LOGIN_FIELDS.username} label='Username' />
-        <FormInput name={LOGIN_FIELDS.password} label='Password' />
+        <FormInput
+          name={LOGIN_FIELDS.username}
+          label='Username'
+          rules={{ required: 'Username is required!' }}
+        />
+        <FormInput
+          name={LOGIN_FIELDS.password}
+          label='Password'
+          rules={{
+            required: 'Password is required!',
+            minLength: {
+              message: 'Use at least 10 characters.',
+              value: 10,
+            },
+          }}
+        />
       </FormProvider>
-      <Button title='Login' />
+      <Button
+        title='Login'
+        onPress={formMethods.handleSubmit(onSubmit, onErrors)}
+      />
     </Wrapper>
   )
 }
