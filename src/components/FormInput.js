b797bcc871ebfa788c9a34f47ebb5ff4f93e5af4
Daniel Koprowski <contact@koprowski.it>
2021-01-26 21:11:42 +0100

    Add FormInput wrapper

--- src/components/FormInput.js
+++ src/components/FormInput.js
@@ -4,14 +4,13 @@ import { useController, useFormContext } from 'react-hook-form'
 
 import { Input } from './Input'
 
-export const FormInput = (props) => {
+const ControlledInput = (props) => {
   const { name, rules, defaultValue = '', ...inputProps } = props
 
   const formContext = useFormContext()
   const { control, errors } = formContext
-  
-  const { field } = useController({ name, control, rules, defaultValue })
 
+  const { field } = useController({ name, control, rules, defaultValue })
 
   return (
     <Input
@@ -24,8 +23,24 @@ export const FormInput = (props) => {
   )
 }
 
+export const FormInput = (props) => {
+  const { name, ...inputProps } = props
+  const formContext = useFormContext()
+
+  if (!formContext || !name) {
+    const errorMessage = !name
+      ? 'Form field must have a "name" prop!'
+      : 'Form field must be a descendant of `FormProvider` as it uses `useFormContext`!'
+    return <Input {...inputProps} error={errorMessage} editable={false} />
+  }
+
+  return <ControlledInput {...props} />
+}
+
 FormInput.propTypes = {
   name: PropTypes.string.isRequired,
   rules: PropTypes.object,
   defaultValue: PropTypes.any,
 }
+
+ControlledInput.propTypes = FormInput.propTypes
